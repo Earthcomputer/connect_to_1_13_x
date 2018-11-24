c4c6fe202945e3d1004c4856281fef8f9236d5eb
Earthcomputer <burtonjae@hotmail.co.uk>
2018-11-24 04:26:27 +0000

    Buildable, and actually works

--- src/main/java/net/earthcomputer/connect_to_1_13_x/ICPacketHandshake.java
+++
@@ -1,5 +0,0 @@
-package net.earthcomputer.connect_to_1_13_x;
-
-public interface ICPacketHandshake {
-    void setProtocolVersion(int version);
-}
--- src/main/java/net/earthcomputer/connect_to_1_13_x/mixin/MixinCPacketHandshake.java
+++ src/main/java/net/earthcomputer/connect_to_1_13_x/mixin/MixinCPacketHandshake.java
@@ -1,7 +1,8 @@
 package net.earthcomputer.connect_to_1_13_x.mixin;
 
-import net.earthcomputer.connect_to_1_13_x.ICPacketHandshake;
 import net.earthcomputer.connect_to_1_13_x.Protocols;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.multiplayer.ServerData;
 import net.minecraft.network.EnumConnectionState;
 import net.minecraft.network.handshake.client.CPacketHandshake;
 import org.spongepowered.asm.mixin.Mixin;
@@ -11,17 +12,18 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(CPacketHandshake.class)
-public class MixinCPacketHandshake implements ICPacketHandshake {
+public class MixinCPacketHandshake {
 
     @Shadow private int protocolVersion;
 
-    @Inject(method = "<init>(Ljava/lang/String;ILnet/minecraft/network/EnumConnectionState;)V", at = @At("TAIL"), remap = false)
+    @Inject(method = "<init>(Ljava/lang/String;ILnet/minecraft/network/EnumConnectionState;)V", at = @At("TAIL"))
     public void onConstruct(String ip, int port, EnumConnectionState requestedState, CallbackInfo ci) {
-        Protocols.switchProtocol(Protocols.PROTOCOL_1_13);
-    }
+        ServerData serverData = Minecraft.getMinecraft().getCurrentServerData();
+        int newProtocol = Protocols.PROTOCOL_1_13;
+        if (serverData != null && Protocols.isProtocolSupported(serverData.version))
+            newProtocol = serverData.version;
 
-    @Override
-    public void setProtocolVersion(int version) {
-        this.protocolVersion = version;
+        protocolVersion = newProtocol;
+        Protocols.switchProtocol(newProtocol);
     }
 }
