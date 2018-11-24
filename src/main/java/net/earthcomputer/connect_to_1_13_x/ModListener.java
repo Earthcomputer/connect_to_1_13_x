8934f608906858edd040c9e951f82a89a3518543
Earthcomputer <burtonjae@hotmail.co.uk>
2018-11-24 01:57:11 +0000

    Initial commit

---
+++ src/main/java/net/earthcomputer/connect_to_1_13_x/ModListener.java
@@ -0,0 +1,10 @@
+package net.earthcomputer.connect_to_1_13_x;
+
+import org.dimdev.riftloader.listener.InitializationListener;
+
+public class ModListener implements InitializationListener {
+    @Override
+    public void onInitialization() {
+        System.out.println("HELLO MINECRAFT!");
+    }
+}
